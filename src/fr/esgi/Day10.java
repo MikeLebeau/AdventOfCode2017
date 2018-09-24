8657958c95c9bb9c03cf441477e01ab9b9612c61
Mike Lebeau <m.lebeau@oodrive.com>
2018-09-24 18:43:08 +0200

    etmoijhev

--- src/fr/esgi/Day10.java
+++ src/fr/esgi/Day10.java
@@ -3,6 +3,7 @@ package fr.esgi;
 import com.sun.deploy.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day10 {
@@ -11,60 +12,75 @@ public class Day10 {
         // On array 256 [0 to 255]
         String input = "70,66,255,2,48,0,54,48,80,141,244,254,160,108,1,41";
 
-        // On array 5[0 to 4]
-        String inputTest = "3,4,1,5";
+        // On array 5 [0 to 4]
+        String inputTest = "1,2,3";
 
         int sizeArray = 256;
-        int skipSize = 0;
 
-        int[] sequenceNum = new int[sizeArray];
-        String[] inputSequence = input.split(",");
+        int[] sequenceNum = createFulfilledIntArray(sizeArray);
 
-        //Fulfill the array
-        for (int i = 0; i < sequenceNum.length; i++) {
-            sequenceNum[i] = i;
-        }
+        String line = convertToAsciiCode(inputTest);
+        line = addSuffix(line);
+		System.out.println("Sequence : " + line);
+        String[] inputSequence = line.split(",44,");
 
-        int startAt = 0;
+//		for (int i = 0; i < 64; i++) {
+//			doProcess(inputSequence, sequenceNum, 0, 0);
+//		}
 
-        for (int i = 0; i < inputSequence.length; i++) {
-            int[] subArray;
+		System.out.println("Sequence : " + Arrays.toString(inputSequence));
+		doProcess(inputSequence, sequenceNum, 0, 0);
 
-            System.out.println("START AT : " + startAt);
-
-            int length = Integer.parseInt(inputSequence[i]);
-
-            subArray = copyOfRange(sequenceNum, startAt, length);
-            subArray = reverseArray(subArray);
-
-            for (int i1 = 0; i1 < subArray.length; i1++) {
-                int index = (i1+startAt)%sequenceNum.length;
-
-                sequenceNum[index] = subArray[i1];
-            }
-
-            startAt += length;
-            startAt += skipSize;
-            skipSize++;
-            printIntArray(sequenceNum);
-        }
-
-//        printIntArray(sequenceNum);
+//      printIntArray(sequenceNum);
         System.out.println(sequenceNum[0] + " x " + sequenceNum[1] + " = " + (sequenceNum[0]*sequenceNum[1]));
 
     }
 
-    static String convert
+    static void doProcess(String[] inputSequence, int[] sequenceNum, int startAt, int skipSize){
+		for (int i = 0; i < inputSequence.length; i++) {
+			int[] subArray;
 
-    static String convertToPrintableChar(String sequence){
-        List<String> result = new ArrayList<>(sequence.split(",").length);
+			int length = Integer.parseInt(inputSequence[i]);
 
-        for (String s : sequence.split(",")) {
-            result.add();
-        }
+			subArray = copyOfRange(sequenceNum, startAt, length);
+			subArray = reverseArray(subArray);
 
-        return StringUtils.join(result, ",");
-    }
+			for (int i1 = 0; i1 < subArray.length; i1++) {
+				int index = (i1+startAt)%sequenceNum.length;
+
+				sequenceNum[index] = subArray[i1];
+			}
+
+			startAt += length;
+			startAt += skipSize;
+			skipSize++;
+			printIntArray(sequenceNum);
+		}
+	}
+
+    static String convertToAsciiCode(String line){
+		String result = "";
+		for (int i = 0; i < line.toCharArray().length; i++) {
+			result += String.valueOf((int)line.charAt(i));
+			result += (i != (line.length()-1)) ? "," : "";
+		}
+
+		return result;
+	}
+
+	static String addSuffix(String line){
+    	return line += ",17,31,73,47,23";
+	}
+
+	static int[] createFulfilledIntArray(int size){
+		int[] array = new int[size];
+    	//Fulfill the array
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
+		}
+
+		return array;
+	}
 
     static void printIntArray(int[] array){
         System.out.println("----------PRINT ARRAY----------");
